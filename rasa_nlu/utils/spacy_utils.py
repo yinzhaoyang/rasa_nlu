8390714f60082a807f4e805622ab36ccac0ff3d6
Thomas Werkmeister <thomas@twerkmeister.com>
2017-11-11 09:35:27 +0800

    commented small assumption out

--- rasa_nlu/utils/spacy_utils.py
+++ rasa_nlu/utils/spacy_utils.py
@@ -118,8 +118,8 @@ class SpacyNLP(Component):
 
         if nlp is None:
             raise Exception("Failed to load spacy language model. Loading the model returned 'None'.")
-        if nlp.path is None:
-            # Spacy sets the path to `None` if it did not load the model from disk.
-            # In this case `nlp` is an unusable stub.
-            raise Exception("Failed to load spacy language model for lang '{}'. ".format(nlp.lang) +
-                            "Make sure you have downloaded the correct model (https://spacy.io/docs/usage/).")
+#        if nlp.path is None:
+#            # Spacy sets the path to `None` if it did not load the model from disk.
+#            # In this case `nlp` is an unusable stub.
+#            raise Exception("Failed to load spacy language model for lang '{}'. ".format(nlp.lang) +
+#                            "Make sure you have downloaded the correct model (https://spacy.io/docs/usage/).")
